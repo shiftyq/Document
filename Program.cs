d2106ccb8b0a9b26e1423993d497975292cb2538
shiftyq <42647199+shiftyq@users.noreply.github.com>
2018-09-20 13:05:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System;
+
+namespace Document
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Document");
+            Console.WriteLine("Please enter the name of the document:  ");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Type some content into this document please: ");
+            string s2 = Console.ReadLine();
+            try
+            {
+
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("Document.txt");
+
+                //Write a line of text
+                sw.WriteLine(s1 + s2);
+
+               
+
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+
+                Console.WriteLine(s1 +  " ", "was successfully saved.The document contains");
+            }
+        }
+    }
+}
